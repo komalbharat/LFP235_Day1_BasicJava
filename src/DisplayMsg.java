6f382ff450e1b1b35d80c045a7c41390851b5069
komalbharat <gharmode.komal@gmail.com>
2023-01-04 22:06:25 +0530

    This Day 1 Assignment Basic Java

---
+++ src/DisplayMsg.java
@@ -0,0 +1,8 @@
+//1.1 Write a program to display any message:
+public class DisplayMsg {
+    public static void main(String[] args) {
+
+        System.out.printf("Welcome To Java");
+    }
+}
+
---
+++ src/StaticNonstaticMethod.java
@@ -0,0 +1,13 @@
+public class StaticNonstaticMethod {
+    public static void main(String args[]) {
+        StaticNonstaticMethod d = new StaticNonstaticMethod();
+        d.add(10,20);     // to call for non-static method
+    }
+
+    public void add(int x ,int y) {
+        int a = x;
+        int b = y;
+        int c = a + b;
+        System.out.println("addition" + c);
+    }
+}
